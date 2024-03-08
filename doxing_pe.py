78e07c3b01f5947fe3bbbf13f04ef067107e674a
edgarluck <76820660+edgarluck@users.noreply.github.com>
2024-03-08 13:40:54 -0500

    Update doxing_pe.py

--- doxing_pe.py
+++ doxing_pe.py
@@ -4,7 +4,7 @@ import subprocess
 from git import Repo
 import requests
 
-VERSION = "1.1.18"
+VERSION = "1.1.19"
 REPO_URL = "https://github.com/edgarluck/DOXING_PE.git"
 TARGET_DIRECTORY = "DOXING_PE"
 
@@ -61,14 +61,14 @@ def menu():
     return opcion
 
 def consultar_dni(dni):
-    response = requests.get(f'https://3aa8-132-191-2-29.ngrok-free.app/api/personas?dni={dni}')
+    response = requests.get(f'https://780b-132-191-2-29.ngrok-free.app/api/personas?dni={dni}')
     if response.status_code == 200:
         print(response.text)
     else:
         print("Error:", response.text)
 
 def consultar_nombres(nombres, apellido_paterno, apellido_materno):
-    response = requests.get(f'http://localhost:8000/api/personas?nombres={nombres}&apellidoPaterno={apellido_paterno}&apellidoMaterno={apellido_materno}')
+    response = requests.get(f'https://780b-132-191-2-29.ngrok-free.app/api/personas?nombres={nombres}&apellidoPaterno={apellido_paterno}&apellidoMaterno={apellido_materno}')
     if response.status_code == 200:
         print(response.text)
     else:
